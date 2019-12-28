cfb9525681c823f59a71bf262dedba9ebf4c1e3f
PENG <2899650241@qq.com>
2019-12-28 11:44:39 +0800

    refactor

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,5 +1,13 @@
 public class FizzBuzz {
+
     public static String of(int num) {
-        return "Fizz";
+        if(num % 3 == 0 && num % 5 != 0){
+            return "Fizz";
+        }else if(num % 5 == 0 && num % 3 != 0){
+            return "Buzz";
+        }else if(num % 3 == 0 && num % 5 == 0){
+            return "FizBuzz";
+        }
+        return num + "";
     }
 }
